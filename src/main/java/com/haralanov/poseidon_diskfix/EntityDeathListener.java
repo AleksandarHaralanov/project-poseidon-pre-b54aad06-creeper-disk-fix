5384916b4e6d6ebd8a09d17c9b28bffc46621f7b
AleksandarHaralanov <aleksandar.haralanov01@gmail.com>
2024-07-07 04:53:21 +0300

    Add whitespace for clarity, organize imports and add variable modifiers

--- src/main/java/com/haralanov/poseidon_diskfix/EntityDeathListener.java
+++ src/main/java/com/haralanov/poseidon_diskfix/EntityDeathListener.java
@@ -1,32 +1,35 @@
-package me.beezle.pdiskfix;
+package com.haralanov.poseidon_diskfix;
+
+import java.util.Random;
 
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDeathEvent;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Creeper;
-import org.bukkit.entity.Skeleton;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Skeleton;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.Random;
-
-public class EntityHandler implements Listener {
-
-    private final Random random = new Random();
+public class EntityDeathListener implements Listener {
 
     @EventHandler
     public void onCreeperDeath(final EntityDeathEvent event) {
-        Entity victim = event.getEntity();
+        final Entity victim = event.getEntity();
+
         if (victim instanceof Creeper) {
             if (victim.getLastDamageCause() instanceof EntityDamageByEntityEvent) {
-                EntityDamageByEntityEvent damage = (EntityDamageByEntityEvent) victim.getLastDamageCause();
+                final EntityDamageByEntityEvent damage = (EntityDamageByEntityEvent) victim.getLastDamageCause();
+
                 if (damage.getDamager() instanceof Arrow) {
-                    Entity shooter = ((Arrow)damage.getDamager()).getShooter();
+                    final Entity shooter = ((Arrow)damage.getDamager()).getShooter();
+
                     if (shooter instanceof Skeleton) {
-                        Material diskType = random.nextBoolean() ? Material.GOLD_RECORD : Material.GREEN_RECORD;
+                        final Random random = new Random();
+                        final Material diskType = random.nextBoolean() ? Material.GOLD_RECORD : Material.GREEN_RECORD;
+
                         victim.getLocation().getWorld().dropItemNaturally(victim.getLocation(), new ItemStack(diskType, 1));
                     }
                 }
